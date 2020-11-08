bc8789e7a671ca3b573d9b88357b6e105946afe0
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-11-08 23:36:05 +0300

    add class Hyena

---
+++ src/main/java/ru/itsjava/oop/Hyena.java
@@ -0,0 +1,39 @@
+package ru.itsjava.oop;
+
+public class Hyena {
+    private String name;
+    private int pawsCount;
+    private int tailCount;
+
+    public Hyena (String name) {
+        this.name = name;
+        pawsCount = 4;
+        tailCount = 1;
+
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setPawsCount(int pawsCount){
+        this.pawsCount = pawsCount;
+    }
+    public int getPawsCount(){
+        return pawsCount;
+    }
+
+    public void setTailCount(int tailCount){
+        this.tailCount = tailCount;
+    }
+
+    public int getTailCount(){
+        return tailCount;
+    }
+
+
+}
