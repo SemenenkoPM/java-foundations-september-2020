19d772e7df2cd387d59ede36a8b130b0e4fe10df
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-11-16 01:54:48 +0300

    wrote "cup"

--- src/main/java/ru/itsjava/homework1/part2/Cup.java
+++ src/main/java/ru/itsjava/homework1/part2/Cup.java
@@ -1,4 +1,16 @@
 package ru.itsjava.homework1.part2;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Cup {
+    String forWhat;
+    String volume;
+    String color;
+    String material;
+
 }
--- src/main/java/ru/itsjava/homework1/part2/Launcher.java
+++ src/main/java/ru/itsjava/homework1/part2/Launcher.java
@@ -1,10 +1,15 @@
 package ru.itsjava.homework1.part2;
 
+import lombok.Setter;
+
 public class Launcher {
 
     public static void main(String[] args) {
 
         Car ford = new Car("Форд", "Зеленая", "65", "R4");
+        Cup cup = new Cup("Чай", "100", "Белый", "стекло");
+
+
 
     }
 }
