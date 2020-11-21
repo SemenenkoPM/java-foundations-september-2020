3fe391663428e4ec09ff8a104a8bc50b6c95bb6b
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-11-21 15:08:27 +0300

    java: cannot reference area before supertype constructor has been called

--- src/main/java/ru/itsjava/homework1/part5/Figure.java
+++ src/main/java/ru/itsjava/homework1/part5/Figure.java
@@ -1,18 +1,17 @@
 package ru.itsjava.homework1.part5;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class Figure {
 
     public int area;
     public int perimetr;
 
-    public Figure(int area, int perimetr) {
-        this.area = area;
-        this.perimetr = perimetr;
-    }
+
 
 }
