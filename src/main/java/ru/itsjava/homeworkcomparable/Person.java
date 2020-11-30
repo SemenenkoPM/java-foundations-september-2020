f4ad98d424632eb44d85f9f26c8aabe578c6f49c
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-11-30 13:14:57 +0300

    revisions compareTo

--- src/main/java/ru/itsjava/homeworkcomparable/Person.java
+++ src/main/java/ru/itsjava/homeworkcomparable/Person.java
@@ -18,13 +18,24 @@ public class Person implements Comparable<Person>, Cloneable{
 
     @Override
     public int compareTo(Person o) {
-        if (surname.equalsIgnoreCase(o.surname))
-            if (name.equalsIgnoreCase(o.name)) {
+        if (surname.compareToIgnoreCase(o.surname) < 0) {
+            return -1;
+        } if (surname.compareToIgnoreCase(o.surname) > 0) {
+            return 1;
+        }
+            else if (name.compareToIgnoreCase(o.name) < 0) {
+            return -1;
+        }
+            if (name.compareToIgnoreCase(o.name) > 0) {
+                return 1;
+            }
+                else if (age > o.age) {
+                    return 1;
+                }
                 if (age == o.age) {
                     return 0;
                 }
-            }
-        return -1;
+                return -1;
     }
 
     @Override
