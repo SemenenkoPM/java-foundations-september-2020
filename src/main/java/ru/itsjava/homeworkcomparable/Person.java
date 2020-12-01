b53af23b362df9aa9a23a1b1015d0e88d7454153
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-12-01 16:51:13 +0300

    change compareTo to Double.compare

--- src/main/java/ru/itsjava/homeworkcomparable/Person.java
+++ src/main/java/ru/itsjava/homeworkcomparable/Person.java
@@ -7,9 +7,9 @@ public class Person implements Comparable<Person>, Cloneable{
 
     private final String name;
     private final String surname;
-    private final int age;
+    private final double age;
 
-    public Person(String name, String surname, int age) {
+    public Person(String name, String surname, double age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
@@ -18,24 +18,25 @@ public class Person implements Comparable<Person>, Cloneable{
 
     @Override
     public int compareTo(Person o) {
-        if (surname.compareToIgnoreCase(o.surname) < 0) {
-            return -1;
-        } if (surname.compareToIgnoreCase(o.surname) > 0) {
-            return 1;
-        }
-            else if (name.compareToIgnoreCase(o.name) < 0) {
-            return -1;
-        }
-            if (name.compareToIgnoreCase(o.name) > 0) {
-                return 1;
-            }
-                else if (age > o.age) {
-                    return 1;
-                }
-                if (age == o.age) {
-                    return 0;
-                }
-                return -1;
+        return Double.compare(age, o.age);
+//        if (surname.compareToIgnoreCase(o.surname) < 0) {
+//            return -1;
+//        } if (surname.compareToIgnoreCase(o.surname) > 0) {
+//            return 1;
+//        }
+//            else if (name.compareToIgnoreCase(o.name) < 0) {
+//            return -1;
+//        }
+//            if (name.compareToIgnoreCase(o.name) > 0) {
+//                return 1;
+//            }
+//                else if (age > o.age) {
+//                    return 1;
+//                }
+//                if (age == o.age) {
+//                    return 0;
+//                }
+//                return -1;
     }
 
     @Override
