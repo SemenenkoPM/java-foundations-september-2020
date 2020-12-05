099a0ff041c4e04773e4a7afdfc842ed33a6521c
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-12-05 16:21:23 +0300

    work in class

---
+++ src/main/java/ru/itsjava/proxy/Main.java
@@ -0,0 +1,11 @@
+package ru.itsjava.proxy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = new User( "");
+        user.setName("Павел");
+
+    }
+}
---
+++ src/main/java/ru/itsjava/proxy/UserProxy.java
@@ -0,0 +1,13 @@
+//package ru.itsjava.proxy;
+//
+//public class UserProxy extends User{
+//
+//    public UserProxy(String name) {
+//        super(name);
+//    }
+//
+//    public void setName(String name) {
+//        super.setName();
+//    }
+//
+//}
