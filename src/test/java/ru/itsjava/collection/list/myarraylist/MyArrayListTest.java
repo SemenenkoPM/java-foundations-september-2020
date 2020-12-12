2b7baf68b9e374648d4408c2dc3749cd288cd0e1
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-12-12 16:12:39 +0300

    work in class

---
+++ src/test/java/ru/itsjava/collection/list/myarraylist/MyArrayListTest.java
@@ -0,0 +1,37 @@
+package ru.itsjava.collection.list.myarraylist;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Класс MyArrayList должен: ")
+public class MyArrayListTest {
+
+    public static final String DEFAULT_ELEM = "Elem 1";
+
+    @DisplayName(" корректно удалять по элементу")
+    @Test
+    public void shouldHaveCorrectRemoveByElement(){
+        MyArrayList list = new MyArrayList();
+        list.add(DEFAULT_ELEM);
+        int sizeBeforeRemove = list.size();
+        list.remove(DEFAULT_ELEM);
+
+        Assertions.assertEquals(sizeBeforeRemove - 1, list.size());
+    }
+
+    @DisplayName(" корректно добавлять по элементу")
+    @Test
+
+    public void shouldHaveCorrectAddByElement(){
+        MyArrayList list = new MyArrayList();
+        list.add(DEFAULT_ELEM);
+        int sizeBeforeAdd = list.size();
+        list.add(DEFAULT_ELEM);
+
+        Assertions.assertEquals(sizeBeforeAdd + 1, list.size());
+
+    }
+
+
+}
