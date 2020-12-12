8eb368285a1ba3d49b0b45c33d018141f82c01f1
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-12-12 16:13:08 +0300

    work in class

---
+++ src/main/java/ru/itsjava/collection/list/mylinkedlist/MyLinkedListPractice.java
@@ -0,0 +1,11 @@
+package ru.itsjava.collection.list.mylinkedlist;
+
+public class MyLinkedListPractice {
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        linkedList.add("first");
+
+
+    }
+
+}
