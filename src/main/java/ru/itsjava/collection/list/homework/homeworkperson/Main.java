60b9b2ede9f109a37a5751b28d796ecb1a5ff4af
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-12-13 20:05:58 +0300

    hw

---
+++ src/main/java/ru/itsjava/collection/list/homework/homeworkperson/Main.java
@@ -0,0 +1,37 @@
+package ru.itsjava.collection.list.homework.homeworkperson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person("Николай", "Петров", true, 20);
+        Person person2 = new Person("Виктор", "Сидоров", true, 22);
+        Person person3 = new Person("Нео", "Сидоров", true, 26);
+        Person person4 = new Person("Виктор", "Сидоров", true, 22);
+        Person person5 = new Person("Ольга", "Петрова", false, 22);
+
+
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+        personList.add(person5);
+
+        // Вернуть всех мужчин, кто проходит по условиям
+
+        System.out.println("person1.itsTimeToCall() = " + person1.itsTimeToCall());
+        System.out.println("person2.itsTimeToCall() = " + person2.itsTimeToCall());
+
+        for (Person person : personList) {
+            if (person.itsTimeToCall() == true) {
+                System.out.println(person);
+            }
+        }
+
+    }
+}
---
+++ src/main/java/ru/itsjava/collection/list/homework/homeworkperson/Person.java
@@ -0,0 +1,34 @@
+package ru.itsjava.collection.list.homework.homeworkperson;
+
+import lombok.Data;
+
+@Data
+public class Person {
+
+    private final String name;
+    private final String surname;
+    private final boolean isMale;
+    private final int age;
+
+    public Person(String name, String surname, boolean isMale, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.isMale = isMale;
+        this.age = age;
+    }
+
+
+
+    public boolean itsTimeToCall(){
+        if(isMale == true){
+            if(age >= 18 & age <= 27){
+                char nameStartsWith = 'Н';
+                if(name.charAt(0) == nameStartsWith){
+                return true;
+                }
+            }
+        }
+      return false;
+    }
+
+}
