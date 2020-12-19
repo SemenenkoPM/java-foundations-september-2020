c189da3649b8b720ff0946798cff63bef639260c
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2020-12-19 14:10:07 +0300

    work in class

--- src/main/java/ru/itsjava/collection/list/mylinkedlist/MyLinkedListPractice.java
+++ src/main/java/ru/itsjava/collection/list/mylinkedlist/MyLinkedListPractice.java
@@ -5,6 +5,18 @@ public class MyLinkedListPractice {
         MyLinkedList linkedList = new MyLinkedList();
         linkedList.add("first");
 
+        System.out.println(linkedList);
+        linkedList.add("second");
+        linkedList.add("third");
+        System.out.println("linkedList.size() = " + linkedList.size());
+        System.out.println("linkedList.remove(1) = " + linkedList.remove(1));
+        System.out.println("linkedList = " + linkedList);
+
+        System.out.println("linkedList.remove(1) = " + linkedList.remove(1));
+//        System.out.println("linkedList.remove(1) = " + linkedList.remove(1));
+
+//        System.out.println("linkedList.remove(1) = " + linkedList.remove(0));
+//        System.out.println("linkedList = " + linkedList);
 
     }
 
