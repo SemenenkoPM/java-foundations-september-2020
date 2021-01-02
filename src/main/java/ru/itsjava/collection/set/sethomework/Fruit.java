b972dd5a8b799a1b18a03254b5f5e756451d04ec
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2021-01-02 19:42:23 +0300

    hw set

---
+++ src/main/java/ru/itsjava/collection/set/sethomework/Fruit.java
@@ -0,0 +1,16 @@
+package ru.itsjava.collection.set.sethomework;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class Fruit {
+
+    private final String name;
+    private double weihgt;
+
+
+
+
+}
